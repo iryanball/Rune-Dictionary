2af01a80b2e062e1c77370ab8c9fe5b16b2720cb
Ryan Ball <ryanball@icloud.com>
2017-12-06 00:06:56 +0000

    Fixes Apple Watch App

--- Rune Dictionary/SettingsViewController.swift
+++ Rune Dictionary/SettingsViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    var allowReversedRunes = true
+    var allowDuplicateRunes = true
+    
+    @IBOutlet weak var allowReversedRunesSwitch: UISwitch!
+    @IBOutlet weak var allowDuplicateRunesSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func reversedRunesChanged(_ sender: Any) {
+        
+        if allowReversedRunesSwitch.isOn {
+            
+            allowReversedRunes = true
+            
+        } else {
+            allowReversedRunes = false
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func duplicateRunesChanged(_ sender: Any) {
+        
+        if allowDuplicateRunesSwitch.isOn {
+            
+            allowDuplicateRunes = true
+            
+        } else {
+            
+            allowDuplicateRunes = false
+            
+        }
+        
     }
-    */
-
+    
 }
